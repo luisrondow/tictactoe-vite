18af8ee876197273491a1bd20ca00b4c74fec255
Luis von Rondow <ext.luis.rondow@farfetch.com>
2023-03-07 10:29:34 +0000

    add pwa configs

---
+++ pwa.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Jogo do Galo",
+  short_name: "JogoDoGalo",
+  theme_color: "#fff",
+  background_color: "#fff",
+  display: "standalone",
+  scope: "/",
+  start_url: "/",
+  icons: [
+    {
+      src: "./icon.png",
+      sizes: [96, 128, 192, 256, 384, 512],
+    },
+  ],
+};
